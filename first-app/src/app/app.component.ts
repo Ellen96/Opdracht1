5db02bc977c9640640bcc8b181ea8305d69e5ada
Ellen96 <61159491+Ellen96@users.noreply.github.com>
2020-03-02 21:28:01 +0100

    einde les

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  teGokkenGetal:number=Math.floor((Math.random() * 120) + 1);
   title = 'first-app';
 }
+
