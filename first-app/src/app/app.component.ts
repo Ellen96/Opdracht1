6748ead419af888421a6b791e380c9c3793a7e48
Ellen96 <61159491+Ellen96@users.noreply.github.com>
2020-03-06 04:25:25 +0100

    if nog goed krijgen

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   teGokkenGetal:number=Math.floor((Math.random() * 120) + 1);
+  count: number=0;
+  ingegevenGetal = +(document.getElementById("ingegevenGetal"));
+  controleAntwoord = document.getElementById("controleAntwoord");
+  clickCount():void{this.count++;
+    if(this.ingegevenGetal==this.teGokkenGetal)
+    { alert("Goed geraden je hebt gewonnen.");}
+    else
+    {
+    alert("Probeer opnieuw.");
+    }
+  };
   title = 'first-app';
+  
 }
-
